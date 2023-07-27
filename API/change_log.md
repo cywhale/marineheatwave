1ad216dec957045474a13ac1f19067dc1f5c8b9d
cywhale <cyrain@gmail.com>
2023-07-27 11:12:57 +0800

    ver 0.1.3 Breaking fix bug conversion to [0,360]/pm2 pre-stop kill gunicorn processes

--- API/change_log.md
+++ API/change_log.md
@@ -4,3 +4,5 @@ ODB API of Marine Heatwaves
 #### ver 0.1.1 deployment on eco.odb.ntu.edu.tw 20230725
 
 #### ver 0.1.2 rewrite zarr to solve nan merged in diff rows problem/use pypolars
+
+#### ver 0.1.3 Breaking fix bug conversion to [0,360]/pm2 pre-stop kill gunicorn processes
--- API/mhw_app.py
+++ API/mhw_app.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import pandas as pd
 import polars as pl
-# import numpy as np
+import numpy as np
 from fastapi import FastAPI, HTTPException, Query, Response
 from fastapi.responses import JSONResponse, FileResponse
 from fastapi.openapi.docs import get_swagger_ui_html
@@ -58,7 +58,9 @@ async def custom_swagger_ui_html():
 ### Global variables ###
 gridSz = 0.25
 timeLimit = 365
-
+LON_RANGE_LIMIT = 90
+LAT_RANGE_LIMIT = 90
+AREA_LIMIT = LON_RANGE_LIMIT * LAT_RANGE_LIMIT
 
 @app.on_event("startup")
 async def startup():
@@ -67,9 +69,20 @@ async def startup():
                       group='anomaly', decode_times=True)
 
 
+def deg2str(value, isLon=True, oriNeg=False, roundTo=3):
+    if (isLon):
+      # if original lon is -180 -> 180 + 360 = 180 -> deg2str -> 180 cannot convert back to -180
+      lon = value - 360 if value > 180 or oriNeg else value
+      return str(round(lon, roundTo)).replace(".", "d")
+
+    return str(round(value, roundTo)).replace(".", "d")
+
 def to_nearest_grid_point(lon: float, lat: float) -> tuple:
-    grid_lon = round(lon * 4) / 4
-    grid_lat = round(lat * 4) / 4
+    mlon = 180 if lon > 180 else (-180 if lon < -180 else lon)
+    mlat = 90 if lat > 90 else (-90 if lat < -90 else lat)
+    grid_lon = round(mlon * 4) / 4
+    grid_lat = round(mlat * 4) / 4
+    grid_lon = grid_lon + 360 if grid_lon < 0 else grid_lon
     return (grid_lon, grid_lat)
 
 
@@ -82,6 +95,9 @@ async def process_mhw_data(lon0: float, lat0: float, lon1: Optional[float], lat1
     if not variables:
         raise HTTPException(
             status_code=400, detail="Invalid variable(s). Allowed variables are 'sst_anomaly', 'level', and 'td'.")
+
+    variables.sort() #in-place sort not return anything
+    out_file = '-'.join(variables)
 
     if start is None:
         start_date = pd.to_datetime('1982-01-01')
@@ -104,28 +120,62 @@ async def process_mhw_data(lon0: float, lat0: float, lon1: Optional[float], lat1
                 status_code=400, detail="Invalid end date format")
 
     try:
+        orig_lon0, orig_lon1 = lon0, lon1
         lon0, lat0 = to_nearest_grid_point(lon0, lat0)
 
         if lon1 is None or lat1 is None:
             # Only one point, no date range limitation
-            data_subset = dz.sel(lon=slice(lon0+180, lon0+180+gridSz-0.01), lat=slice(
+            out_file = (out_file + '_' + deg2str(lon0, True, orig_lon0<=-179.875) + '_' + deg2str(lat0, False) +
+                        '_' + 'Point' + '_' + str(start_date.date()) + '_' + str(end_date.date()))
+            data_subset = dz.sel(lon=slice(lon0, lon0+gridSz-0.01), lat=slice(
                 lat0, lat0+gridSz-0.01), date=slice(start_date, end_date))
 
         else:
-            # Bounding box, 1 year date range limitation
+            # Bounding box, 1 month or 1 year date range limitation
             lon1, lat1 = to_nearest_grid_point(lon1, lat1)
-            if (end_date - start_date).days > timeLimit:
+
+            # Adjust temporal range limit based on spatial range
+            lon_range = abs(orig_lon1 - orig_lon0)
+            lat_range = abs(lat1 - lat0)
+            area_range = lon_range * lat_range
+
+            if (lon_range > LON_RANGE_LIMIT and lat_range > LAT_RANGE_LIMIT) or (area_range > AREA_LIMIT):
+                # Large spatial range, limit to one month of data
+                end_date = start_date + pd.DateOffset(months=1) - timedelta(days=1)
+            elif (end_date - start_date).days > timeLimit:
+                # Smaller spatial range, limit to one year of data
                 end_date = start_date + timedelta(days=timeLimit)
 
-            data_subset = dz.sel(lon=slice(
-                lon0+180, lon1+180), lat=slice(lat0, lat1), date=slice(start_date, end_date))
+            if lon0 > lon1 and np.sign(orig_lon0) == np.sign(orig_lon1):
+                # Swap if lon0 > lon1 but the same sign
+                lon0, lon1 = lon1, lon0
+                orig_lon0, orig_lon1 = orig_lon1, orig_lon0
+
+            if np.sign(orig_lon0) != np.sign(orig_lon1):
+                # Requested area crosses the zero meridian
+                if orig_lon1 < 0:
+                    # Swap if orig_lon1 < 0 and now 180 < lon1 < 360
+                    lon0, lon1 = lon1, lon0
+                    orig_lon0, orig_lon1 = orig_lon1, orig_lon0
+
+                subset1 = dz.sel(lon=slice(lon0, 360), lat=slice(lat0, lat1), date=slice(start_date, end_date))
+                subset2 = dz.sel(lon=slice(0, lon1), lat=slice(lat0, lat1), date=slice(start_date, end_date))
+                data_subset = xr.concat([subset1, subset2], dim='lon')
+            else:
+                # Requested area doesn't cross the zero meridian
+                data_subset = dz.sel(lon=slice(lon0, lon1), lat=slice(lat0, lat1), date=slice(start_date, end_date))
+
+            out_file = (out_file + '_' + deg2str(lon0, True, orig_lon0<=-179.875) + '_' + deg2str(lat0, False) +
+                        '_' + deg2str(lon1, True, orig_lon1<=-179.875) + '_' + deg2str(lat1, False) +
+                        '_' + 'BBOX' + '_' + str(start_date.date()) + '_' + str(end_date.date()))
 
         if data_subset.nbytes == 0:
             raise HTTPException(
                 status_code=400, detail="No data available for the given parameters.")
 
         df = data_subset.to_dataframe().reset_index()
-        df['lon'] = df['lon'] - 180
+        mask = df['lon'] > 180
+        df.loc[mask, 'lon'] = df.loc[mask, 'lon'] - 360
         df = df[['lon', 'lat', 'date'] +
                 variables].dropna(how='all', subset=variables)
 
@@ -139,7 +189,7 @@ async def process_mhw_data(lon0: float, lat0: float, lon1: Optional[float], lat1
         ##df.replace([np.inf, -np.inf], np.nan, inplace=True)  # Replace infinite values with NaN
         ##df.fillna("", inplace=True)  # Replace NaN values with None #not "null"
         # return df #Pandas version
-        return pl.from_pandas(df)
+        return out_file, pl.from_pandas(df)
 
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -178,7 +228,7 @@ async def read_mhw(
     * Bonding-box MHWs with 1-year time-span limitation: e.g. api/mhw?lon0=135&lon1&=140&lat0=15&lat1=30&start=2021-01-01
     """
 
-    df = await process_mhw_data(lon0, lat0, lon1, lat1, start, end, append)
+    _, df = await process_mhw_data(lon0, lat0, lon1, lat1, start, end, append)
     ##return JSONResponse(content=df.to_dict(orient='records')) #cannot handle NA when JSONResponse
     # return JSONResponse(content=json.loads(df.to_json(orient='records'))) # work version in Pandas
     return JSONResponse(content=df.to_dicts())
@@ -207,10 +257,9 @@ async def read_mhw_csv(
     * Bonding-box MHWs with 1-year time-span limitation: e.g. api/mhw?lon0=135&lon1&=140&lat0=15&lat1=30&start=2021-01-01
     """
 
-    df = await process_mhw_data(lon0, lat0, lon1, lat1, start, end, append)
+    out_file, df = await process_mhw_data(lon0, lat0, lon1, lat1, start, end, append)
     temp_file = NamedTemporaryFile(delete=False)
     # df.to_csv(temp_file.name, index=False) #Pandas solution, work
     df.write_csv(temp_file.name) #polars version
 
-    startx = '1982-01-01' if start is None else str(start)
-    return FileResponse(temp_file.name, media_type="text/csv", filename="mhw_" + startx + ".csv")
+    return FileResponse(temp_file.name, media_type="text/csv", filename=out_file+".csv")
