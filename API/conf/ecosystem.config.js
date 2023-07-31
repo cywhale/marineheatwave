7db4f568d733766f91f339e79589ae82327eeb54
cywhale <cyrain@gmail.com>
2023-07-31 14:55:46 +0800

    ver 0.1.4 Breaking add SST/Climatology plot API/Use Dask-scheduler/worker

--- API/conf/ecosystem.config.js
+++ API/conf/ecosystem.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  apps : [{
+  apps : [
+  {
+    name: 'dask-scheduler',
+    script: 'dask-scheduler &',
+    autorestart: true,
+    restart_delay: 5000,
+  },
+  {
+    name: 'dask-worker',
+    script: 'dask-worker tcp://localhost:8786 &',
+    autorestart: true,
+    restart_delay: 10000,
+  },
+  {
     name: 'mhwapi',
     script: 'gunicorn mhw_app:app -w 4 -k uvicorn.workers.UvicornWorker -b 127.0.0.1:8020 --keyfile conf/privkey.pem --certfile conf/fullchain.pem --reload',
     args: '',
@@ -12,6 +25,6 @@ module.exports = {
     append_env_to_name: true,
     watch: false,
     max_memory_restart: '4G',
-    pre_stop: 'kill $(ps aux | grep \'mhw_app\' | awk \'{print $2}\')',
+    pre_stop: 'kill $(ps aux | grep \'mhw_app\' | awk \'{print $2}\') && kill $(ps aux | grep \'dask-scheduler\' | awk \'{print $2}\') && kill $(ps aux | grep \'dask-worker\' | awk \'{print $2}\')',
   }],
 };
--- API/conf/simu.sh
+++ API/conf/simu.sh
@@ -1,12 +1,19 @@
 # install packages
 pip3 install -r ./requirements.txt
 
+# run dask-scheduler and dask-worker
+dask-scheduler --port 8786 &
+dask-worker tcp://localhost:8786 &
+
 # run API server
 ## localhost: gunicorn mhw_app:app -k uvicorn.workers.UvicornWorker -b 127.0.0.1:8020
 gunicorn mhw_app:app -w 4 -k uvicorn.workers.UvicornWorker -b 127.0.0.1:8020 --keyfile conf/privkey.pem --certfile conf/fullchain.pem --reload
 
 # kill process
 ps -ef | grep 'gunicorn' | grep -v grep | awk '{print $2}' | xargs -r kill -9
+
+# kill dask
+kill $(ps aux | grep \'dask-scheduler\' | awk \'{print $2}\') && kill $(ps aux | grep \'dask-worker\' | awk \'{print $2}\')
 
 # pm2 start
 pm2 start ./conf/ecosystem.config.js
